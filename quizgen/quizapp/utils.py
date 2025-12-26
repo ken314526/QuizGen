c565fdf5ba4be9c0dceeae246ebbdd749ba0400b
Abhishek Sharma <abhisheksharma801831@gmail.com>
2025-12-26 21:42:00 +0530

    Code optimization

--- quizgen/quizapp/utils.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import get_user_model
-
-
-def get_admin_user():
-    try:
-        User = get_user_model()
-        return User.objects.filter(is_superuser=True).first().pk
-    except Exception as ex:
-        return None
